4334d0f5f98fe3786e81ed884f150adeecd94a84
MMAI-HYU <163245058+MMAI-HYU@users.noreply.github.com>
2026-03-30 15:39:34 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   // GitHub Pages: repo 이름에 맞춰 수정 (e.g., '/paper-tracker/')
   // username.github.io 루트에 배포하면 '/' 로 두면 됨
-  base: "/paper_db/",
+  base: "/paper-tracker/",
 });
